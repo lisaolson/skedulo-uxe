ff9604ccbe26a7f54e9453d9d41fea60eb90c018
Lisa <olson.lisa94@gmail.com>
2022-04-12 09:47:54 -0600

    - Updates naming of objects

--- pwa/serviceWorker.js
+++ pwa/serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const staticDevEmployee = "dev-employee-site"
 const assets = [
   "/",
   "/index.html",
@@ -17,7 +17,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevEmployee).then(cache => {
       cache.addAll(assets)
     })
   )
